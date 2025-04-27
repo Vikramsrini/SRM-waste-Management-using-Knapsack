305f1a5c6f2994f7f2b45ec59fda1f6c75ba0640
Vikramsrini <73245931+Vikramsrini@users.noreply.github.com>
2025-04-27 23:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ async function optimizeCollection() {
     optimizeBtn.disabled = true;
     
     try {
-        const response = await fetch('https://012c-103-197-112-241.ngrok-free.app//optimize', {
+        const response = await fetch('https://81d5-103-197-112-241.ngrok-free.app//optimize', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
